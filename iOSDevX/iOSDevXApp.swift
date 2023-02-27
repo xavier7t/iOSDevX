3e2e0f72c746bd6d3552a59c9043790d49bdea6b
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-26 21:33:54 -0500

    Initial Commit

---
+++ iOSDevX/iOSDevXApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDevXApp.swift
+//  iOSDevX
+//
+//  Created by Xavier on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSDevXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
