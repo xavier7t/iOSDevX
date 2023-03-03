a41ed4e5fa56695d6d37476ccd8d4fd92bafa07f
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-03-02 22:19:03 -0500

    DemoGroup20230302

---
+++ iOSDevX/202303-Mar 2023/Group/ContentView-DemoGroup20230302.swift
@@ -0,0 +1,143 @@
+//
+//  ContentView-DemoGroup20230302.swift
+//  iOSDevX
+//
+//  Created by Xavier on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView_DemoGroup20230302: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Grouped")
+                        Spacer()
+                        Text("Ungrouped")
+                    }
+                    .bold()
+                    .font(.title3)
+                    Divider()
+                    HStack {
+                        groupedElements
+                        Spacer()
+                        ungroupedElements
+                    }
+                    Divider()
+                    HStack {
+                        groupedForModifier
+                        Spacer()
+                        ungroupedForModifier
+                    }
+                    Divider()
+                }
+            }
+            .padding(.horizontal)
+            .navigationTitle("GroupView")
+        }
+    }
+}
+
+struct ContentView_DemoGroup20230302_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView_DemoGroup20230302()
+    }
+}
+
+
+//MARK: Using a group to avoid extra argument in call
+extension ContentView_DemoGroup20230302 {
+    private var ungroupedElements: some View {
+        VStack {
+            Text("Text1")
+            Text("Text2")
+            Text("Text3")
+            Text("Text4")
+            Text("Text5")
+            Text("Text6")
+            Text("Text7")
+            Text("Text8")
+            Text("Text9")
+            Text("Text10")
+            //Uncomment the line below to see the error message
+            //Text("Text11")
+        }
+    }
+    private var groupedElements: some View {
+        VStack {
+            Group {
+                Text("Text1")
+                Text("Text2")
+                Text("Text3")
+                Text("Text4")
+                Text("Text5")
+                Text("Text6")
+            }
+            Group {
+                Text("Text7")
+                Text("Text8")
+                Text("Text9")
+                Text("Text10")
+                Text("Text11")
+            }
+        }
+    }
+}
+
+//MARK: Using a group to create a conditional view
+extension ContentView_DemoGroup20230302 {
+    //Uncomment the property below to see the error message
+//    private var ungroupedIfStatement: some View {
+//        if true {
+//            Text("I’m true")
+//        }
+//    }
+    private var groupedIfStatement: some View {
+        Group {
+            if true {
+                Text("I’m true")
+            }
+        }
+    }
+}
+
+
+//MARK: Using a group to apply a view modifier to multiple views
+extension ContentView_DemoGroup20230302 {
+    private var ungroupedForModifier: some View {
+        VStack {
+            Text("I want to be bold.")
+                .bold()
+                .font(.footnote)
+                .onTapGesture {
+                    print("Tapped1")
+                }
+            Text("I want to be bold.")
+                .bold()
+                .font(.footnote)
+                .onTapGesture {
+                    print("Tapped2")
+                }
+        }
+    }
+    
+    private var groupedForModifier: some View {
+        VStack {
+            Group {
+                Text("I want to be bold.")
+                Text("I want to be bold.")
+                Text("I want to be bold.")
+                Text("I want to be bold.")
+                Text("I want to be bold.")
+                Text("I want to be bold.")
+            }
+            .bold()
+            .font(.footnote)
+            .onTapGesture {
+                print("Tapped")
+            }
+        }
+    }
+}
