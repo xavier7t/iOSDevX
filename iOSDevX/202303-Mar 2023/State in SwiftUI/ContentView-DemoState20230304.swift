a1a16ff6711faa96237267964c27a66e6e11cdac
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-03-04 22:11:24 -0500

    DemoState20230304

--- iOSDevX/202303-Mar 2023/State in SwiftUI/ContentView-DemoState20230304.swift
+++ iOSDevX/202303-Mar 2023/State in SwiftUI/ContentView-DemoState20230304.swift
@@ -8,24 +8,38 @@
 import SwiftUI
 
 struct ContentView_DemoState20230304: View {
-    //var myString: String = ""
+//    var myString: String = ""
     @State private var stateString1: String = "Hello, World! - 1"
     private var stateString2: State<String> = State(initialValue: "Hello, World! - 2")
     var body: some View {
         VStack {
-            //Text(myString)
+//            Text(myString)
             Text(stateString1)
+            HStack {
+                Text("User input: ")
+                TextField("Enter your username", text: stateString2.projectedValue)
+                    .underline()
+            }
             Text(stateString2.wrappedValue)
             Text(stateString2.projectedValue.wrappedValue)
         }
         .onAppear {
-            //myString = "Show this in the contentview"
+//            myString = "Show this in the contentview"
             stateString1 = "Content View on appear..."
             stateString2.wrappedValue = "Content View on appear..."
-            print("Projected value: ", stateString2.projectedValue)
+            print("Projected value: ", stateString2.projectedValue.wrappedValue)
             print("Wrapped value: ", stateString2.wrappedValue)
         }
     }
+    
+    //Uncomment the struct below to replicate the error
+//    struct TestStruct {
+//        var myString: String = ""
+//        var someComputedProperty: String {
+//            myString = "Hello world"
+//            return ""
+//        }
+//    }
 }
 
 struct ContentView_DemoState20230304_Previews: PreviewProvider {
