e005923d9e70ca2f1b9c14377dcf7501325d126f
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-03-08 21:35:38 -0500

    DemoGridView20230308

---
+++ iOSDevX/202303-Mar 2023/Grid View/ContentView-DemoGridView20230308.swift
@@ -0,0 +1,142 @@
+//
+//  ContentView-DemoGridView20230308.swift
+//  iOSDevX
+//
+//  Created by Xavier on 3/8/23.
+//
+
+import SwiftUI
+
+struct ContentView_DemoGridView20230308: View {
+    private var flexibleGridItems: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible())
+    ]
+    private var fixedGridItems: [GridItem] = [
+        .init(.fixed(50)),
+        .init(.fixed(50)),
+        .init(.fixed(50))
+    ]
+    private var adaptiveGridItems: [GridItem] = [
+        .init(.adaptive(minimum: 120, maximum: 150)),
+        .init(.adaptive(minimum: 150)),
+        .init(.adaptive(minimum: 120))
+    ]
+    private var homeGridItems: [GridItem] = [
+        .init(.fixed(150))
+    ]
+    var body: some View {
+            TabView {
+                homeView
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                flexibleGridItemsView
+                    .tabItem {
+                        Label("Flexible", systemImage: "figure.flexibility")
+                    }
+                fixedGridItemsView
+                    .tabItem {
+                        Label("Fixed", systemImage: "photo.artframe")
+                    }
+                adaptiveGridItemsView
+                    .tabItem {
+                        Label("Adaptive", systemImage: "poweroutlet.type.a")
+                    }
+
+            }
+    }
+}
+
+struct ContentView_DemoGridView20230308_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView_DemoGridView20230308()
+    }
+}
+
+extension ContentView_DemoGridView20230308 {
+
+    private var flexibleGridItemsView: some View {
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVGrid(columns: flexibleGridItems) {
+                    ForEach(0...9, id: \.self) { item in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke()
+                                .frame(width: 100, height: 150)
+                                .foregroundColor(.accentColor)
+                            Text("\(item)")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Flexible")
+        }
+    }
+    
+    private var fixedGridItemsView: some View {
+        NavigationStack {
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: fixedGridItems) {
+                    ForEach(0...9, id: \.self) { item in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke()
+                                .frame(width: 100, height: 150)
+                                .foregroundColor(.accentColor)
+                            Text("\(item)")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Fixed")
+        }
+
+    }
+    
+    private var adaptiveGridItemsView: some View {
+        NavigationStack {
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: adaptiveGridItems) {
+                    ForEach(0...9, id: \.self) { item in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke()
+                                .frame(width: 100, height: 150)
+                                .foregroundColor(.accentColor)
+                            Text("\(item)")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Adaptive")
+        }
+    }
+    private var homeView: some View {
+        NavigationStack {
+            ScrollView {
+                HStack(alignment: .top) {
+                    LazyVGrid(columns: homeGridItems) {
+                        imageList
+                    }
+                    LazyVGrid(columns: homeGridItems) {
+                        imageList
+                    }
+                }
+            }
+            .navigationTitle("Grid View")
+        }
+    }
+    
+    private var imageList: some View {
+        ForEach(0...49, id: \.self) { _ in
+            Image("demo")
+                .resizable()
+                .clipped()
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .frame(width: 180, height: CGFloat.random(in: 180...300), alignment: .center)
+        }
+    }
+}
