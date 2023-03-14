cf8c5622f2c9179f71db6fa511a057b4395c1548
Xavier <yuxuanli.work@icloud.com>
2023-03-13 23:01:20 -0400

    DemoPicker20230313

--- iOSDevX/202303-Mar 2023/Pickers in SwiftUI/ContentView-DemoPicker20230313.swift
+++ iOSDevX/202303-Mar 2023/Pickers in SwiftUI/ContentView-DemoPicker20230313.swift
@@ -8,34 +8,34 @@
 import SwiftUI
 
 struct ContentView_DemoPicker20230313: View {
+    
     @State private var pickedDirection: Direction = .north
-    var body: some View {
-        NavigationStack {
-            List {
-                picker
-                picker.pickerStyle(.menu)
-                picker.pickerStyle(.navigationLink)
-                picker.pickerStyle(.segmented)
-                picker.pickerStyle(.wheel)
-                picker.pickerStyle(.inline)
-            }
-            .navigationTitle("Picker View")
-        }
-    }
     enum Direction: String, CaseIterable {
         case north = "North"
         case south = "South"
         case east = "East"
         case west = "West"
     }
-    
-    private var picker: some View {
-        VStack {
-            Picker("Select a direction", selection: $pickedDirection) {
-                ForEach(Direction.allCases, id: \.self) { direction in
-                    Text(direction.rawValue)
-                }
+    var picker: some View {
+        Picker("Select a direction", selection: $pickedDirection) {
+            ForEach(Direction.allCases, id: \.self) { direction in
+                Text(direction.rawValue)
             }
+        }
+    }
+    var body: some View {
+        NavigationStack {
+            List {
+                picker
+                picker.pickerStyle(.menu)
+                picker
+                    .pickerStyle(.segmented)
+                picker
+                    .pickerStyle(.wheel)
+                picker
+                    .pickerStyle(.inline)
+            }
+            .navigationTitle("SwiftUI Pickers")
         }
     }
 }
